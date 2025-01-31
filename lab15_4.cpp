699632625e3b43e2f5e532a5e0b8731c3f649094
Teetad <teetadraseesit@gmail.com>
2025-02-01 00:22:02 +0700

    complete

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &,int &,int &);
+void shuffle(int *,int *,int *,int *);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,7 +11,7 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d);
+	    shuffle(&a,&b,&c,&d);
 	    cout << a << " " << b << " " << c << " " << d << endl;
 	}
 	
@@ -19,3 +19,15 @@ int main(){
 }
 
 //Do not modify source code above this line
+
+void shuffle (int *a, int *b, int *c, int *d){
+    int angpao[4] = {50,  100,  500, 1000};
+   for (int i = 0; i < 4; i++) {
+        int r = rand() % 4;
+        swap(angpao[i], angpao[r]);
+    }
+    *a = angpao[0];
+    *b = angpao[1];
+    *c = angpao[2];
+    *d = angpao[3];
+}
